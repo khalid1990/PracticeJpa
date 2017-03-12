31e1a5eb5fc3cc6f7c16d3b61e282c2f458c387c
khalid016 <babar_odesk@yahoo.com>
2017-03-13 02:46:47 +0600

    Added ExamCategory, ExamSubCategory, Tag values for BCS

--- src/main/java/com/babar/common/enums/ExamCategory.java
+++ src/main/java/com/babar/common/enums/ExamCategory.java
@@ -5,4 +5,18 @@ package com.babar.common.enums;
  * @since 3/12/17.
  */
 public enum ExamCategory {
+    BCS("BCS Preliminary Examination"),
+    MEDICAL_ADMISSION_TEST("Medical Admission Test"),
+    UNIVERSITY_ADMISSION_TEST("University Admission Test"),
+    COMPUTER_SCIENCE_AND_ENGINEERING("Computer Science And Engineering");
+
+    String name;
+
+    ExamCategory(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/main/java/com/babar/common/enums/ExamSubCategory.java
+++ src/main/java/com/babar/common/enums/ExamSubCategory.java
@@ -1,8 +1,43 @@
 package com.babar.common.enums;
 
+import static com.babar.common.enums.ExamCategory.*;
+
 /**
  * @author babar
  * @since 3/12/17.
  */
 public enum ExamSubCategory {
+    BENGALI("Bengali Language and Literature", 1, BCS),
+    ENGLISH("English Language and Literature", 2, BCS),
+    BANGLADESH_AFFAIRS("National Affair", 3, BCS),
+    INTERNATIONAL_AFFAIRS("International Affair", 4, BCS),
+    GEOGRAPHY_ENVIRONMENT("Geography Environment and Disaster Management", 5, BCS),
+    GENERAL_SCIENCE("General Science", 6, BCS),
+    MATH("Mathematical Reasoning", 7, BCS),
+    MENTAL_ABILITY("Mental Ability", 8, BCS),
+    ETHICS("Ethics Values and Good Governance ", 9, BCS);
+
+    String name;
+
+    int id;
+
+    ExamCategory examCategory;
+
+    ExamSubCategory(String name, int id, ExamCategory examCategory) {
+        this.name = name;
+        this.id = id;
+        this.examCategory = examCategory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public ExamCategory getExamCategory() {
+        return examCategory;
+    }
 }
